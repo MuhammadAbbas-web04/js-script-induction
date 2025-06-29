3583c417e334690a8048fd893c22c890b860a231
ibrahim82-magsi <muhammad.ibrahim.magsi@gmail.com>
2025-06-29 17:31:13 +0500

    added

--- scrip.js
+++ scrip.js
@@ -1,4 +1,52 @@
-console.log("hello world");
-console.log("muhammad abbas");
-console.log("student of smit");
-console.log("bacth3");
+// console.log("hello world");
+// console.log("muhammad abbas");
+// console.log("student of smit");
+// console.log("bacth3");
+
+// // alert("this is  a warning");
+
+// console.log("hello world");
+// console.log(120 + 730);
+// console.log("29");
+
+// var username = "muhmmadabbas";
+// console.log(username);
+// username = "abdullah";
+// console.log(username);
+// var city = "quetta";
+// console.log(city);
+// var age = "23";
+// console.log(age);
+// console.log(username);
+
+// var username = 123;
+// console.log(username);
+
+// var username = "abuzar";
+// console.log()
+var totelMarks=400;
+var username=prompt( "inter your name" );
+var subject1=+prompt ( " enter your subject 1 marks " ) ;
+var subject2=+prompt ( " enter your subject 2 marks " ) ;
+var subject3=+prompt ( " enter your subject  3 marks " ) ;
+var subject4=+prompt ( " enter your subject 4 marks " ) ;
+var obtainMarks=  subject1 + subject2 + subject3 + subject4 ;
+var percentage= obtainMarks/totelMarks*100;
+console.log("Mr/Mrs " + username + " your percentage is: " + percentage + "%");  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
